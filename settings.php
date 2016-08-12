2fdae039e634a25edda7d03a3aab154ee4b1e3f8
Robert Puffer <puffro01@9135.local>
2016-08-12 13:32:30 -0500

    updated code for recent courses block

---
+++ settings.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die;
+/*
+if ($ADMIN->fulltree) {
+    $settings->add(new admin_setting_configtext('block_recent_courses_timetosee', get_string('timetosee', 'block_recent_courses'),
+                   get_string('configtimetosee', 'block_recent_courses'), 5, PARAM_INT));
+}
+*/
+
